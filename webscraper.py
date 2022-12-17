04f97a5d0bab1444b84bc3473edb90c272d92240
Jason  Ouyang <jasonouyang@Jasons-MBP.attlocal.net>
2022-12-16 22:34:57 -0800

    setup to access courses category

--- webscraper.py
+++ webscraper.py
@@ -1,1 +1,14 @@
-## Script to acccess College Categories
+## Script to acccess College Categoriescl
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+import time
+
+driver = webdriver.Chrome(r"./driver/chromedriver")
+driver.get("https://mycollege-guest.laccd.edu/psc/classsearchguest/EMPLOYEE/HRMS/c/COMMUNITY_ACCESS.CLASS_SEARCH.GBL?Campus=ELAC&strm=2232")
+driver.maximize_window()
+
+## Access Courses Category
+button = driver.find_element(By.NAME, "CLASS_SRCH_WRK2_SSR_PB_SUBJ_SRCH$0")
+button.click()
+time.sleep(10)
